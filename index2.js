82a250f6651ebf519d285004ea82b7bcef807272
ming <curyowo@gmail.com>
2019-05-23 20:25:01 +0900

    allright

--- index2.js
+++ index2.js
@@ -3,7 +3,13 @@
 const express = require('express');
 const app = express();
 const exec = require('child_process').exec;
+const algor_select= ["luhn" , "edmundson ", "lsa", "text-rank", "lex-rank", "sum-basic", "kl"];
+let algorithm = "text-rank";
+let length = "3";
+let response;
+const check = /[ㄱ-ㅎ|ㅏ-ㅣ|가-힣]/;
 
+// if (check.test(event.message.text.substring(0, 100))) {
 app.use(express.json());
 
 app.get('/summary', (req, res) => {
@@ -12,8 +18,41 @@ app.get('/summary', (req, res) => {
 app.post('/summary', (req, res) => {
    console.log('post!');
    let inputData = req.body;
-   console.log(inputData);
-   res.write("TEST!");
+   let text = inputData['text'];
+   length = inputData['number'];
+   console.log(text);
+
+   // text 요약
+   if(text.length <= 500) {
+      response = "Need more text sizes"
+      res.write(response);
+   } else {
+     // 한글일 경우
+     if(check.test(text.substring(0,100))) {
+       console.log("한글 요약" + text);
+       exec("sumy " + algorithm + " --length=" + length + " --language=korean --text=" + '"' + text + '"', (error, stdout, stderr) => {
+        console.log('stdout: ' + stdout);
+        console.log('stderr: ' + stderr);
+        if (error !== null) {
+            console.log('exec error: ' + error);
+        }
+        response = stdout;
+        res.write(response);
+    });
+     }
+     // 영어일 경우
+     else {
+      exec("sumy " + algorithm + " --length=" + length + " --language=en --text=" + '"' + text + '"', (error, stdout, stderr) => {
+        console.log('stdout: ' + stdout);
+        console.log('stderr: ' + stderr);
+        if (error !== null) {
+            console.log('exec error: ' + error);
+        }
+        response = stdout;
+        res.write(response);
+    });
+     }
+   }
    res.status(200).end();
 });
 
