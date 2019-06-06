fa4cc9d81dfe272320ec0896bd2eb67ebacfe92b
ming <curyowo@gmail.com>
2019-06-06 17:47:26 +0900

    sss

--- index2.js
+++ index2.js
@@ -44,7 +44,7 @@ app.post('/summary', (req, res) => {
             console.log('exec error: ' + error);
         }
         response = stdout;
-        res.set({ 'content-type': 'text/plain' });
+        res.set({'content-type': 'text/html'});
         res.status(200).send(response);
     });
      }
@@ -57,7 +57,7 @@ app.post('/summary', (req, res) => {
             console.log('exec error: ' + error);
         }
         response = stdout;
-        res.set({ 'content-type': 'text/plain' });
+        res.set({'content-type': 'text/html'});
         res.status(200).send(response);
     });
      }
